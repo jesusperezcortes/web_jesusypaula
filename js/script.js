ec10d1453e9834a3d3b7f1134a7a3e1234f81172
jesusperezcortes <jesuspedro.perez@alumnos.udit.es>
2025-11-18 17:40:18 +0100

    parte brands y js del swiper

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
 
+var swiperFeatured = new Swiper(".featured-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+
+var swiperBrand = new Swiper(".brand-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        991: {
+            slidesPerView: 5,
+        },
+    },
+});
